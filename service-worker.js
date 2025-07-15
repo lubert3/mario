221999808ce054327d1b3a6a034b8f803d6bf7f4
lubert3 <l.berteramo@gmail.com>
2025-07-15 19:52:01 +0200

    v2

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+﻿const CACHE_NAME = "mini-runner-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+
+// Installazione e caching iniziale
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Recupero dalla cache o fetch online
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Pulizia vecchie cache
+self.addEventListener("activate", event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (!cacheWhitelist.includes(key)) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
